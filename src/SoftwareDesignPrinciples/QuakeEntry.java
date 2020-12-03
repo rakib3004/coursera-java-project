726f8bd1f6082022058c827a367a1de5f11d0aa8
pc <bsse1129@iit.du.ac.bd>
2020-12-04 02:00:10 +0600

    add new getter and setter value and constructor in QuakeEntry.java

--- src/SoftwareDesignPrinciples/QuakeEntry.java
+++ src/SoftwareDesignPrinciples/QuakeEntry.java
@@ -2,6 +2,7 @@ package SoftwareDesignPrinciples;
 
 public class QuakeEntry {
 
+
     private Location myLocation;
     private String title;
     private double depth;
@@ -14,5 +15,52 @@ public class QuakeEntry {
         this.depth = depth;
         this.magnitude = magnitude;
     }
+    public QuakeEntry(double lat, double lon, double magnitude, String title, double depth) {
+        this.myLocation = new Location(lat,lon);
+        this.title = title;
+        this.depth = depth;
+        this.magnitude = magnitude;
+    }
 
+    public Location getMyLocation() {
+        return myLocation;
+    }
+
+    public void setMyLocation(Location myLocation) {
+        this.myLocation = myLocation;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getDepth() {
+        return depth;
+    }
+
+    public void setDepth(double depth) {
+        this.depth = depth;
+    }
+
+    public double getMagnitude() {
+        return magnitude;
+    }
+
+    public void setMagnitude(double magnitude) {
+        this.magnitude = magnitude;
+    }
+
+    public static class Location {
+        private double lat;
+        private double lon;
+
+        public Location(double lat, double lon) {
+            this.lat = lat;
+            this.lon = lon;
+        }
+    }
 }
